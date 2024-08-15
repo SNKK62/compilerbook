1a921ddd15c70e8c1125689c9f3d026083390bbf
SNKK62 <koki.seno62@gmail.com>
2024-08-15 19:12:17 +0900

    add test case

--- 9cc/test.sh
+++ 9cc/test.sh
@@ -100,6 +100,7 @@ assert 10 'int main() {int x; int *y; int **z; y = &x; z = &y; **z = 5; return x
 assert 4 'int main() {int x; int *y; int *z; y = &x; z = y; z = z + 1; return z-y;}'
 assert 8 'int main() {int x; int *y; int **z; int **zz; y = &x; z = &y; zz = z + 1; return zz-z;}'
 
+assert 4 'int main() {sizeof 1;}'
 assert 4 'int main() {sizeof(1);}'
 assert 4 'int main() {int x; sizeof(x);}'
 assert 4 'int main() {int x; sizeof(x+3);}'
