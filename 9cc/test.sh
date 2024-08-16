cc159506222a6098430186701f3d5a258230ec01
SNKK62 <koki.seno62@gmail.com>
2024-08-16 11:41:22 +0900

    run test in container

--- 9cc/test.sh
+++ 9cc/test.sh
@@ -110,9 +110,8 @@ assert 4 'int main() {int *x; int y; y = 2; x = &y; sizeof(*x);}'
 
 assert 0 'int main() {int x[5]; return 0;}'
 
-# assert 3 'int main() {int a[2]; *a = 1; *(a + 1) = 2; int *p; p = a; return *p + *(p + 1);}'
-
-# assert 3 'int main() {int a[2]; a[0] = 1; a[1] = 2; return a[0] + a[1];}'
+assert 3 'int main() {int a[2]; *a = 1; *(a + 1) = 2; int *p; p = a; return *p + *(p + 1);}'
+assert 5 'int main() {int a[2]; a[0] = 3; a[1] = 2; return a[0] + a[1];}'
 
 echo OK
 
